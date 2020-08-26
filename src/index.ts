3004ad48ff5f613a5150af2d936697fee08be7c4
Gabriela Dutra <gabriela_ddutra@outlook.com>
2020-08-26 15:48:54 -0300

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import {sumNumber} from "./sum"
+
+console.log(" A soma dos numeros é:" + sumNumber(2,5,2))
+console.log("Edit")
---
+++ src/sum.ts
@@ -0,0 +1,3 @@
+export function sumNumber(num1: number, num2: number, num3 : number){
+    return num1 + num2 + num3
+}
